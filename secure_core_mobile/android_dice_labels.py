71ae52aa2a78160be5c6bcac5655ccd8c41800db
Chatnoir01 <168500853+Chatnoir01@users.noreply.github.com>
2026-09-23 06:52:20 +0200

    SECURE CORE: add android_dice_labels.py

---
+++ secure_core_mobile/android_dice_labels.py
@@ -0,0 +1,11 @@
+"""Android Profile for DICE labels used by Secretkeeper policy paths.
+
+Values are pinned from the AOSP Secretkeeper VTS/dice_policy sources.
+"""
+AUTHORITY_HASH = -4670549
+CONFIG_DESC = -4670548
+KEY_MODE = -4670551
+COMPONENT_NAME = -70002
+COMPONENT_VERSION = -70003
+SECURITY_VERSION = -70005
+EXPLICIT_KEY_DICE_CERT_CHAIN_VERSION = 1
